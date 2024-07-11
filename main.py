bde89cff28f1d1a759f73fe857186ed1500e774d
unknown <22127060@student.hcmus.edu.vn>
2024-07-11 11:46:04 +0700

    Thêm chức năng coi lịch sử giao dịch của từng tài khoản bằng view_account_transaction.html và hàm xử lí BE trong main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from app.blueprints.register_account import register_account_bp  
 from app.blueprints.deposit_money import deposit_money_bp 
 from app.blueprints.view_accounts import view_accounts_bp 
@@ -39,6 +39,40 @@ def account_detail(maSo):
     }
     return jsonify(account_data)
 
+@app.route('/view_account_transaction')
+def view_account_transaction():
+    account_id = request.args.get('ID');
+    sort = request.args.get('sort', 'ID_tai_khoan')
+    order = request.args.get('order', 'asc')
 
+    # Ánh xạ các cột hợp lệ để tránh SQL injection
+    valid_columns = {
+        'STT': 'ID_tai_khoan',
+        'Mã Số': 'ID_tai_khoan',  # Giả sử mã số là ID_tai_khoan
+        'Khách Hàng': 'Ho_ten',
+        'Loại Giao Dịch': 'Loai_giao_dich',
+        'Số tiền giao dịch': 'So_tien_giao_dich',
+        'Ngày Giao Dịch': 'Ngay_giao_dich'
+    }
+
+    # Kiểm tra xem cột sort có hợp lệ không
+    if sort not in valid_columns:
+        sort = 'STT'
+
+    try:
+        order_by = f"{valid_columns[sort]} {order.upper()}"
+        query = f"""select ID_tai_khoan, Ho_ten, Loai_giao_dich, So_tien_giao_dich, Ngay_giao_dich from giao_dich gd
+        join tai_khoan_tiet_kiem tk on gd.Tai_khoan_giao_dich = tk.ID_tai_khoan
+        join khach_hang kh on tk.Nguoi_so_huu = kh.Chung_minh_thu
+        where Tai_khoan_giao_dich = '{account_id}'
+        ORDER BY {order_by};"""
+        cursor = db.get_cursor()
+        cursor.execute(query)
+        accounts = cursor.fetchall()
+        print ("accounts: ", accounts)
+        
+        return render_template('view_account_transaction.html', accounts=accounts, ID_returned = account_id)
+    except Exception as e:
+        return jsonify({'message': 'An error occurred', 'error': str(e)})
 if __name__ == '__main__':
     app.run(debug=True)

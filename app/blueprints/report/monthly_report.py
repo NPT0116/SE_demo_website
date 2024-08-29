90ec7751ce48e7b2ad01a6622a8a7a5d41156530
NPT0116 <thanh1612004@gmail.com>
2024-08-30 00:10:37 +0700

    adding graph file

--- app/blueprints/report/monthly_report.py
+++ app/blueprints/report/monthly_report.py
@@ -28,26 +28,28 @@ def submit_monthly_report():
     except Exception as e:
         return jsonify({'message': 'Không thể gửi báo cáo hàng tháng', 'error': str(e)})
 
-
-# @monthly_report_bp.route('/get_info', methods=['POST'])
-# def get_monthly_chart():
-#     try:
-#         data = request.json
-#         loai_tiet_kiem = data['loai_tiet_kiem']
-#         thang = data['thang']
-#         query = f"call ngay_mo_so_dong_so('{thang}', N'{loai_tiet_kiem}')"
-#         cursor = db.get_cursor()
-#         cursor.execute(query)
-#         monthly_reports = cursor.fetchall()
-#         result = [
-#             {
-#                 "ngay": row[0].day,
-#                 "mo_so": row[1],
-#                 "dong_so": row[2]
-#             }
-#             for row in monthly_reports
-#         ]
-#         print (result)
-#         return jsonify(result)
-#     except Exception as e:
-#         return jsonify({'message': 'Không thể gửi báo cáo hàng tháng', 'error': str(e)}), 500
+@monthly_report_bp.route('/chart', methods=['GET'])
+def monthly_chart_report():
+    return render_template('report/monthly_chart.html')
+@monthly_report_bp.route('/get_info', methods=['POST'])
+def get_monthly_chart():
+    try:
+        data = request.json
+        loai_tiet_kiem = data['loai_tiet_kiem']
+        thang = data['thang']
+        query = f"call ngay_mo_so_dong_so('{thang}', N'{loai_tiet_kiem}')"
+        cursor = db.get_cursor()
+        cursor.execute(query)
+        monthly_reports = cursor.fetchall()
+        result = [
+            {
+                "ngay": row[0].day,
+                "mo_so": row[1],
+                "dong_so": row[2]
+            }
+            for row in monthly_reports
+        ]
+        print (result)
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({'message': 'Không thể gửi báo cáo hàng tháng', 'error': str(e)}), 500
